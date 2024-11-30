7f2815cacd48d3223cc9acd88cf2b3fdda72cd87
FARUK2059 <omarfaruk34552@gmail.com>
2024-11-30 19:03:32 +0600

    add user Local grardian schema

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,52 +1,29 @@
 
-// import { Model, Types } from 'mongoose';
+// User name interface
+export type TUserName = {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+};
 
-// export type TUserName = {
-//     firstName: string;
-//     middleName: string;
-//     lastName: string;
-// }
+// Guardian Interface
+export type TGuardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContactNo: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContactNo: string;
+};
 
-// export type TGuardian = {
-//     fatherName: string;
-//     fatherOccupation: string;
-//     fatherContactNo: string;
-//     motherName: string;
-//     motherOccupation: string;
-//     motherContactNo: string;
-// }
+// Local Guardian Interface
+export type TLocalGuardian = {
+    name: string;
+    occupation: string;
+    contactNo: string;
+    address: string;
+};
 
-// export type TLocalGuardian = {
-//     name: string;
-//     occupation: string;
-//     contactNo: string;
-//     address: string;
-// }
-
-// export type TStudent = {
-//     id: string;
-//     user: Types.ObjectId;
-//     password: string;
-//     name: TUserName;
-//     gender: 'male' | 'female' | 'other';
-//     dateOfBirth?: string;
-//     email: string;
-//     contactNo: string;
-//     emergencyContactNo: string;
-//     bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
-//     presentAddress: string;
-//     permanentAddress: string;
-//     guardian: TGuardian;
-//     localGuardian: TLocalGuardian;
-//     profileImg?: string;
-//     isDeleted: boolean;
-// }
-
-// //for creating static
-
-// export interface StudentModel extends Model<TStudent> {
-//     isuserExists(id: string): Promise<TStudent | null>;
-// }
 
 
 
