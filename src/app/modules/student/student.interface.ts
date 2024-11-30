45306127f6c61798c7d2f580f9b3b03bee57dd90
FARUK2059 <omarfaruk34552@gmail.com>
2024-11-30 19:20:25 +0600

    add up student schema

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,3 +1,4 @@
+import { Model, Types } from "mongoose";
 
 // User name interface
 export type TUserName = {
@@ -24,47 +25,71 @@ export type TLocalGuardian = {
     address: string;
 };
 
-
-
-
-// ************************************************************************
-
-export type UserName = {
-    firstName: string;
-    middleName?: string;
-    lastName: string;
-};
-
-export type Guardian = {
-    fatherName: string;
-    fatherOccupation: string;
-    fatherContactNo: string;
-    motherName: string;
-    motherOccupation: string;
-    motherContactNo: string;
-};
-
-export type LocalGuardian = {
-    name: string;
-    occupation: string;
-    contactNo: string;
-    address: string;
-};
-
-export type Student = {
+// Student interface
+export type TStudent = {
     id: string;
-    name: UserName;
+    // user: Types.ObjectId;
+    password: string;
+    name: TUserName;
     gender: 'male' | 'female' | 'other';
     dateOfBirth?: string;
     email: string;
     contactNo: string;
     emergencyContactNo: string;
-    bloogGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+    bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
     presentAddress: string;
-    permanentAddress: string; // Fixed Typo Here
-    guardian: Guardian;
-    localGuardian: LocalGuardian;
+    permanentAddress: string;
+    guardian: TGuardian;
+    localGuardian: TLocalGuardian;
     profileImg?: string;
-    isActive: 'active' | 'blocked';
-    isDeleted: boolean; // Assuming it's in your code
+    isDeleted: boolean;
 };
+
+export interface StudentModel extends Model<TStudent> {
+    isUserExists(id: string): Promise<TStudent | null>;
+}
+
+
+
+
+// ************************************************************************
+
+// export type UserName = {
+//     firstName: string;
+//     middleName?: string;
+//     lastName: string;
+// };
+
+// export type Guardian = {
+//     fatherName: string;
+//     fatherOccupation: string;
+//     fatherContactNo: string;
+//     motherName: string;
+//     motherOccupation: string;
+//     motherContactNo: string;
+// };
+
+// export type LocalGuardian = {
+//     name: string;
+//     occupation: string;
+//     contactNo: string;
+//     address: string;
+// };
+
+// export type Student = {
+//     id: string;
+//     name: UserName;
+//     gender: 'male' | 'female' | 'other';
+//     dateOfBirth?: string;
+//     email: string;
+//     contactNo: string;
+//     emergencyContactNo: string;
+//     bloogGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+//     presentAddress: string;
+//     permanentAddress: string; // Fixed Typo Here
+//     guardian: Guardian;
+//     localGuardian: LocalGuardian;
+//     profileImg?: string;
+//     isActive: 'active' | 'blocked';
+//     isDeleted: boolean; // Assuming it's in your code
+// };
--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,5 +1,5 @@
 import { Schema } from "mongoose";
-import { TGuardian, TLocalGuardian, TUserName } from "./student.interface";
+import { StudentModel, TGuardian, TLocalGuardian, TStudent, TUserName } from "./student.interface";
 
 // User/student Name Schema
 const userNameSchema = new Schema<TUserName>({
@@ -70,6 +70,81 @@ const localGuradianSchema = new Schema<TLocalGuardian>({
         required: [true, 'Address is required'],
     },
 });
+
+// full student schema
+const studentSchema = new Schema<TStudent, StudentModel>(
+    {
+        id: {
+            type: String,
+            required: [true, 'ID is required'],
+            unique: true,
+        },
+        // user: {
+        //     type: Schema.Types.ObjectId,
+        //     required: [true, 'User id is required'],
+        //     unique: true,
+        //     ref: 'User',
+        // },
+        name: {
+            type: userNameSchema,
+            required: [true, 'Name is required'],
+        },
+        gender: {
+            type: String,
+            enum: {
+                values: ['male', 'female', 'other'],
+                message: '{VALUE} is not a valid gender',
+            },   // many or type value inpute use is enum 
+            required: [true, 'Gender is required'],
+        },
+        dateOfBirth: { type: String },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        contactNo: { type: String, required: [true, 'Contact number is required'] },
+        emergencyContactNo: {
+            type: String,
+            required: [true, 'Emergency contact number is required'],
+        },
+        bloodGroup: {
+            type: String,
+            enum: {
+                values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+                message: '{VALUE} is not a valid blood group',
+            },
+        },
+        presentAddress: {
+            type: String,
+            required: [true, 'Present address is required'],
+        },
+        permanentAddress: {
+            type: String,
+            required: [true, 'Permanent address is required'],
+        },
+        guardian: {
+            type: guardianSchema,
+            required: [true, 'Guardian information is required'],
+        },
+        localGuardian: {
+            type: localGuradianSchema,
+            required: [true, 'Local guardian information is required'],
+        },
+        profileImg: { type: String },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        timestamps: true,
+        versionKey: false
+    },
+);
 
 
 
