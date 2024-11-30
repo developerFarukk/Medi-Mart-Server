b74e7c5f6250b68c63e29694b1c92a88b77817c3
FARUK2059 <omarfaruk34552@gmail.com>
2024-11-30 19:55:01 +0600

    up student service

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,30 +1,38 @@
+import { TStudent } from "./student.interface";
+import { Student } from "./student.model";
 
-import { Student } from './student.interface';
-import { StudentModel } from './student.model';
 
 
-const createStudentIntoDB = async (student: Student) => {
-    const result = await StudentModel.create(student);
+// const createStudentIntoDB = async (student: TStudent) => {
+//     const result = await StudentModel.create(student);
+//     return result;
+// };
+
+const createStudentIntoDB = async (studentData: TStudent) => {
+    if (await Student.isUserExists(studentData.id)) {
+        throw new Error('User already exists!');
+    }
+    const result = await Student.create(studentData);
     return result;
 };
 
 const getAllStudentsFromDB = async () => {
-    const result = await StudentModel.find();
+    const result = await Student.find();
     return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-    const result = await StudentModel.findById(id);
+    const result = await Student.findById(id);
     return result;
 };
 
 const deleteStudentFromDB = async (id: string) => {
-    const result = await StudentModel.findByIdAndDelete(id);
+    const result = await Student.findByIdAndDelete(id);
     return result;
 };
 
-const updateStudent = async (id: string, data: Student) => {
-    const result = await StudentModel.findByIdAndUpdate(id, data, {
+const updateStudent = async (id: string, data: TStudent) => {
+    const result = await Student.findByIdAndUpdate(id, data, {
         new: true,
     })
     return result
