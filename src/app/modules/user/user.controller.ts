5c36ecf10040da75ad295fcb4d005ab1392f9f6f
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-02 01:01:58 +0600

    catchAsync user controlar setup

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,31 +1,24 @@
 
-
-import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 
-const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { password, student: studentData } = req.body;
+const createStudent = catchAsync(async (req, res ) => {
 
-        // const zodParsedData = studentValidationSchema.parse(studentData);
+    const { password, student: studentData } = req.body;
 
-        const result = await UserServices.createStudentIntoDB(
-            password,
-            studentData,
-        );
+    // const zodParsedData = studentValidationSchema.parse(studentData);
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: 'Creat Student and user data is succesfully',
-            data: result,
-        });
-    } catch (err) {
-        next(err)
-    }
-};
+    const result = await UserServices.createStudentIntoDB( password, studentData );
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Creat Student and user data is succesfully',
+        data: result,
+    })
+});
 
 export const UserControllers = {
     createStudent,
