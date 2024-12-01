5fbcb1cdd53eda439182f1314a07aa588c936011
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-01 14:54:37 +0600

    modify service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,68 +1,15 @@
 
-// import httpStatus from 'http-status';
 
-// import { Request, Response } from 'express';
-// // import sendResponse from '../../utils/sendResponse';
-// import { UserServices } from './user.service';
-
-// const createStudent = async (
-//     req: Request,
-//     res: Response,
-//     // next: NextFunction,
-// ) => {
-//     try {
-//         const { password, student: studentData } = req.body;
-//         console.log(studentData);
-
-
-//         // const zodParsedData = studentValidationSchema.parse(studentData);
-
-//         const result = await UserServices.createStudentIntoDB(
-//             password,
-//             studentData,
-//         );
-
-//         // console.log(UserServices.createStudentIntoDB);
-
-
-
-
-//         res.status(200).json({
-//             success: true,
-//             message: 'Student is created succesfully',
-//             data: result,
-//         });
-//     } catch (error) {
-//         res.json({
-//             success: false,
-//             message: 'Validation failed',
-//             error: error.message || error,
-//         })
-//     }
-// };
-
-// export const UserControllers = {
-//     createStudent,
-// };
-
-
-
-
-
-
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { UserServices } from './user.service';
 
 const createStudent = async (
     req: Request,
     res: Response,
-    next: NextFunction,
+
 ) => {
     try {
         const { password, student: studentData } = req.body;
-        // console.log(req.body);
-        
-        
 
         // const zodParsedData = studentValidationSchema.parse(studentData);
 
@@ -80,7 +27,7 @@ const createStudent = async (
         res.json({
             success: false,
             message: 'Validation failed',
-            error: error.message || error,
+            error: error
         })
     }
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,44 +1,4 @@
 
-// import config from '../../config';
-// import { TStudent } from '../student/student.interface';
-// import { Student } from '../student/student.model';
-// import { TUser } from './user.interface';
-// import { User } from './user.model';
-
-// const createStudentIntoDB = async (password: string, studentData: TStudent) => {
-//     // create a user object
-//     const userData: Partial<TUser> = {};
-
-//     //if password is not given , use deafult password
-//     userData.password = password || (config.default_password as string);
-
-//     //set student role
-//     userData.role = 'student';
-
-//     //set manually generated it
-//     userData.id = '2030100001';
-
-//     // create a user
-//     const newUser = await User.create(userData);
-
-//     //create a student
-//     if (Object.keys(newUser).length) {
-//         // set id , _id as user
-//         studentData.id = newUser.id;
-//         studentData.user = newUser._id; //reference _id
-
-//         const newStudent = await Student.create(studentData);
-
-
-//         return newStudent;
-//     }
-// };
-
-// export const UserServices = {
-//     createStudentIntoDB,
-// };
-
-
 import config from '../../config';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
@@ -46,7 +6,7 @@ import { TUser } from './user.interface';
 import { User } from './user.model';
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
-    try {
+   
         // Create a user object
         const userData: Partial<TUser> = {};
 
@@ -57,7 +17,7 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
         userData.role = 'student';
 
         // Manually generate ID (consider if you need to generate this dynamically)
-        userData.id = '2030100001';
+        userData.id = '2030100002';
 
         // Create a user
         const newUser = await User.create(userData);
@@ -69,12 +29,11 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
 
         // Create a student only if user creation was successful
         if (Object.keys(newUser).length) {
-            // Set id and _id as user reference for the student
+
             studentData.id = newUser.id;
             studentData.user = newUser._id; // Reference _id for user
-
-            // Log studentData to ensure it's correct before creating
-            console.log('Creating student with data:', studentData);
+          
+            // console.log('Creating student with data:', studentData);
 
             const newStudent = await Student.create(studentData);
 
@@ -87,10 +46,7 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
         } else {
             throw new Error('User creation failed');
         }
-    } catch (error) {
-        console.error('Error creating student into DB:', error.message);
-        throw error; // Rethrow to be handled by the controller
-    }
+    
 };
 
 export const UserServices = {
