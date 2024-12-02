5079f0a9603036ffe7eca8314dd440b0a742924e
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-02 21:58:22 +0600

    ac semister data creat success

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,17 +2,22 @@
 import { Router } from 'express';
 import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/acSemester.route';
 
 const routers = Router();
 
 const moduleRoutes = [
     {
         path: '/users',
-        route: UserRoutes ,
+        route: UserRoutes,
     },
     {
         path: '/students',
         route: StudentRoutes,
+    },
+    {
+        path: '/academic-semesters',
+        route: AcademicSemesterRoutes,
     },
 ];
 
