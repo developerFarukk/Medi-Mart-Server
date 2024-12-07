782d53422a55bcdd3acd1ddedef79857d37b8ed6
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-07 18:55:39 +0600

    delet student data success and prt 3 complet

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import httpStatus from 'http-status';
+import mongoose from "mongoose";
 import { TStudent } from "./student.interface";
 import { Student } from "./student.model";
+import AppError from "../../errors/AppError";
+import { User } from "../user/user.model";
 
 
 
@@ -43,10 +49,10 @@ const getSingleStudentFromDB = async (id: string) => {
     return result;
 };
 
-const deleteStudentFromDB = async (id: string) => {
-    const result = await Student.findByIdAndDelete(id);
-    return result;
-};
+// const deleteStudentFromDB = async (id: string) => {
+//     const result = await Student.findByIdAndDelete(id);
+//     return result;
+// };
 
 // const updateStudent = async (id: string, data: TStudent) => {
 //     const result = await Student.findByIdAndUpdate(id, data, {
@@ -54,6 +60,44 @@ const deleteStudentFromDB = async (id: string) => {
 //     })
 //     return result
 // }
+
+
+const deleteStudentFromDB = async (id: string) => {
+    const session = await mongoose.startSession();
+
+    try {
+        session.startTransaction();
+
+        const deletedStudent = await Student.findOneAndUpdate(
+            { id },
+            { isDeleted: true },
+            { new: true, session },
+        );
+
+        if (!deletedStudent) {
+            throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+        }
+
+        const deletedUser = await User.findOneAndUpdate(
+            { id },
+            { isDeleted: true },
+            { new: true, session },
+        );
+
+        if (!deletedUser) {
+            throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+
+        return deletedStudent;
+    } catch (err) {
+        await session.abortTransaction();
+        await session.endSession();
+        throw new Error('Failed to delete student');
+    }
+};
 
 
 const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
