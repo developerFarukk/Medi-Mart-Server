dd0749adbb1997bd1cf0a72c73e335b8e2527c65
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-08 02:14:14 +0600

    invalid error success

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -9,6 +9,7 @@ import { ZodError } from 'zod';
 import handleZodError from '../errors/handleZodError';
 import config from '../config';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
@@ -29,6 +30,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         errorSources = simplifiedError?.errorSources;
     } else if (err?.name === 'ValidationError') {
         const simplifiedError = handleValidationError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    } else if (err?.name === 'CastError') {
+        const simplifiedError = handleCastError(err);
         statusCode = simplifiedError?.statusCode;
         message = simplifiedError?.message;
         errorSources = simplifiedError?.errorSources;
