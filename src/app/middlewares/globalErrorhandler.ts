1b049f18f0074c1832bf1e0224fc760fd2386a0f
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-08 01:19:28 +0600

    genaral error json model setup

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,17 +2,28 @@
 // /* eslint-disable no-undef */
 // /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 
-const globalErrorHandler : ErrorRequestHandler = ( err, req, res, next) => {
-    const statusCode = 500;
-    const message = err.message || 'Something went to be wrong!';
+import { ErrorRequestHandler } from 'express';
+import { TErrorSources } from '../interface/error';
 
-    res.status(statusCode).json({
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+
+    let statusCode = 500;
+    let message = 'Something went to be wrong!';
+
+    let errorSources: TErrorSources = [
+        {
+            path: '',
+            message: 'Something went wrong',
+        },
+    ];
+
+    return res.status(statusCode).json({
         success: false,
         message,
-        error: err,
+        errorSources,
+        err
+        // error: err,
     });
 };
 
