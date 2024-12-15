314e0efc4dd87825f930bc0be173adc46bcf380d
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-15 21:25:53 +0600

    add Auth Interface and Validation

---
+++ src/app/modules/Auth/auth.validation.ts
@@ -0,0 +1,37 @@
+
+
+import { z } from 'zod';
+
+// LogIn Validation
+const loginValidationSchema = z.object({
+    body: z.object({
+        id: z.string({ required_error: 'Id is required.' }),
+        password: z.string({ required_error: 'Password is required' }),
+    }),
+});
+
+
+// Change Password validation
+const changePasswordValidationSchema = z.object({
+    body: z.object({
+        oldPassword: z.string({
+            required_error: 'Old password is required',
+        }),
+        newPassword: z.string({ required_error: 'Password is required' }),
+    }),
+});
+
+// Refress Token Validation
+const refreshTokenValidationSchema = z.object({
+    cookies: z.object({
+        refreshToken: z.string({
+            required_error: 'Refresh token is required!',
+        }),
+    }),
+});
+
+export const AuthValidation = {
+    loginValidationSchema,
+    changePasswordValidationSchema,
+    refreshTokenValidationSchema,
+};
