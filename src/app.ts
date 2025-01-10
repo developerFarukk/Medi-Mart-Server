1b8f9158bee3a82b3758c8ae113001ed56c92999
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-11 01:23:06 +0600

    up server

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,8 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use( cors({ origin: ['http://localhost:5001'] }) );
+// app.use( cors({ origin: ['http://localhost:5001'] }) );
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 // application routes
 app.use('/api/v1', router);
