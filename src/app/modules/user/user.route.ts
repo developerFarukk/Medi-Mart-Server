dd63f677fc1e06258e769b4a5b9f0050eb0c97d2
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-20 05:38:16 +0600

    update admin create

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -9,13 +9,14 @@ import auth from '../../middlewares/auth';
 import { USER_ROLE } from './user.constant';
 import { UserValidation } from './user.validation';
 import { upload } from '../../utils/sendImageToCloudinary';
+import { User } from './user.model';
 
 const router = express.Router();
 
 // Admin Creat Route
 router.post(
   '/create-admin',
-  auth(USER_ROLE.superAdmin),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
