1d11683d7823b77faf6333c84293576cf47a280c
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-03 00:12:11 +0600

    ad model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,4 @@
+import { Model } from "mongoose";
 
 
 export interface TUser {
@@ -10,3 +11,15 @@ export interface TUser {
     isDeleted: boolean;
     address: string;
 };
+
+
+
+export interface UserModel extends Model<TUser> {
+
+    //instance methods for checking if passwords are matched
+    isPasswordMatched(
+        plainTextPassword: string,
+        hashedPassword: string,
+    ): Promise<boolean>;
+
+}
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,78 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+import config from "../../config";
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema<TUser>(
+    {
+
+        name: {
+            type: String,
+            required: [true, 'user name is required'],
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'user Email is required'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password id is required'],
+            select: 0
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'customers'],
+        },
+        status: {
+            type: String,
+            enum: ['in-progress', 'blocked'],
+            default: 'in-progress',
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        address: {
+            type: String,
+
+        }
+    },
+    {
+        timestamps: true,
+        // versionKey: false
+    },
+);
+
+
+userSchema.pre('save', async function (next) {
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this; // doc
+    // hashing password and save into DB
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_rounds),
+    );
+    next();
+});
+
+// set '' after saving password
+userSchema.post('save', function (doc, next) {
+    doc.password = '';
+    next();
+});
+
+
+// Password Matched
+userSchema.statics.isPasswordMatched = async function (
+    plainTextPassword,
+    hashedPassword,
+) {
+    return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+
+
+export const User = model<TUser>('User', userSchema);
