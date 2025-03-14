ed5db6bf8bb1576ebbec8475f9798cea06f4361d
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-14 16:34:55 +0600

    order managment success

--- src/app/modules/orders/order.service.ts
+++ src/app/modules/orders/order.service.ts
@@ -1,76 +1,5 @@
 
 
-// import { TOrder } from "./order.interface";
-// import { TJwtPayload } from "../auth/auth.interface";
-// import mongoose from "mongoose";
-// import { Medicin } from "../medicines/medicine.model";
-// import Order from "./order.model";
-
-// // Create Order 
-// const createOrderIntoDB = async (
-//     orderData: Partial<TOrder>,
-//     authUser: TJwtPayload
-// ) => {
-//     const session = await mongoose.startSession();
-//     session.startTransaction();
-//     try {
-//         if (orderData.products) {
-//             let totalPrice = 0;
-//             let totalQuantity = 0;
-
-//             for (const medicinItem of orderData.products) {
-//                 const product = await Medicin.findById(medicinItem.medicins).session(session);
-
-//                 if (product) {
-//                     if (product.quantity < medicinItem.orderQuantity) {
-//                         throw new Error(`Insufficient stock for product: ${product.name}`);
-//                     }
-
-//                     // Decrement the product stock
-//                     product.quantity -= medicinItem.orderQuantity;
-//                     await product.save({ session });
-
-//                     // Calculate subTotalPrice for each product
-//                     medicinItem.subTotalPrice = product.price * medicinItem.orderQuantity;
-
-//                     // Add to totalPrice
-//                     totalPrice += medicinItem.subTotalPrice;
-//                     totalQuantity = medicinItem.orderQuantity
-//                 } else {
-//                     throw new Error(`Product not found: ${medicinItem.medicins}`);
-//                 }
-//             }
-
-//             // Set totalPrice in orderData
-//             orderData.totalPrice = totalPrice;
-//             orderData.totalQuantity = totalQuantity
-//         }
-
-//         const order = new Order({
-//             ...orderData,
-//             user: authUser.userId,
-//         });
-
-//         console.log(order);
-
-
-//         const createdOrder = await order.save({ session });
-//         await session.commitTransaction();
-//         session.endSession();
-
-//         return createdOrder;
-//     } catch (error) {
-//         await session.abortTransaction();
-//         session.endSession();
-//         throw error;
-//     }
-// };
-
-// export const OrderService = {
-//     createOrderIntoDB,
-// };
-
-
 import { TOrder } from "./order.interface";
 import { TJwtPayload } from "../auth/auth.interface";
 import mongoose from "mongoose";
@@ -87,7 +16,7 @@ const createOrderIntoDB = async (
     try {
         if (orderData.products) {
             let totalPrice = 0;
-            let totalQuantity = 0; // মোট quantity হিসাব করার জন্য ভেরিয়েবল
+            let totalQuantity = 0;
 
             for (const medicinItem of orderData.products) {
                 const product = await Medicin.findById(medicinItem.medicins).session(session);
@@ -108,7 +37,7 @@ const createOrderIntoDB = async (
                     totalPrice += medicinItem.subTotalPrice;
 
                     // Add to totalQuantity
-                    totalQuantity += medicinItem.orderQuantity; // প্রতিটি প্রোডাক্টের orderQuantity যোগ করা হচ্ছে
+                    totalQuantity += medicinItem.orderQuantity; 
                 } else {
                     throw new Error(`Product not found: ${medicinItem.medicins}`);
                 }
@@ -116,7 +45,7 @@ const createOrderIntoDB = async (
 
             // Set totalPrice and totalQuantity in orderData
             orderData.totalPrice = totalPrice;
-            orderData.totalQuantity = totalQuantity; // মোট quantity সেট করা হচ্ছে
+            orderData.totalQuantity = totalQuantity;
         }
 
         const order = new Order({
