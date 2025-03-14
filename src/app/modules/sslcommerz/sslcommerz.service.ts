d67fa6a6e92d4e2208fa2e4e96e78cf5b2efbca7
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-15 03:34:38 +0600

    add data 345

--- src/app/modules/sslcommerz/sslcommerz.service.ts
+++ src/app/modules/sslcommerz/sslcommerz.service.ts
@@ -1,2 +1,68 @@
 
+import express from 'express';
+import config from "../../config";
 
+const app = express();
+
+const store_id = config.store_id as string;
+const store_passwd = config.store_passwd as string;
+const is_live = config.is_live as string;
+// const is_live = false; 
+
+const initPayment = async (paymentData: { total_amount: number, tran_id: string }) => {
+    const { total_amount, tran_id } = paymentData;
+
+    const data = {
+        total_amount,
+        currency: 'BDT',
+        tran_id, // Use unique tran_id for each API call
+        success_url: `${config.ssl.validation_url}?tran_id=${tran_id}`,
+        fail_url: config.ssl.failed_url as string,
+        cancel_url: config.ssl.cancel_url as string,
+        ipn_url: 'http://next-mart-steel.vercel.app/api/v1/ssl/ipn',
+        shipping_method: 'Courier',
+        product_name: 'N/A.',
+        product_category: 'N/A',
+        product_profile: 'general',
+        cus_name: 'N/A',
+        cus_email: 'N/A',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'N/A',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+    };
+
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+
+    try {
+        const apiResponse = await sslcz.init(data);
+
+        // Redirect the user to the payment gateway
+        const GatewayPageURL = apiResponse.GatewayPageURL;
+
+        if (GatewayPageURL) {
+            return GatewayPageURL;
+        } else {
+            throw new AppError(StatusCodes.BAD_GATEWAY, "Failed to generate payment gateway URL.");
+        }
+    } catch (error) {
+        throw new AppError(StatusCodes.INTERNAL_SERVER_ERROR, "An error occurred while processing payment.");
+    }
+};
+
+
+export const sslService = {
+    initPayment,
+    // validatePaymentService
+};
